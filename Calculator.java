22ed1c6691fff001a0ad5c35373d6d796607223d
Benjamin Joseph <benjaminjoseph2002@gmail.com>
2021-11-29 14:57:22 +0530

    Calculator addition

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+public class Calculator
+{
+    public static void main(String[] args)
+    {
+        String symbol = args[0];
+        float n1 = Float.parseFloat(args[1]);
+        float n2 = Float.parseFloat(args[2]);
+        switch(symbol)
+        {
+            case "add": System.out.println("Sum = "+(n1+n2));
+            break;
+            case "sub": System.out.println("Difference = "+(n1-n2));
+            break;
+            case "mul": System.out.println("Product = "+(n1*n2));
+            break;
+            case "div": System.out.println("Quotient = "+(n1/n2));
+            break;
+            default: System.out.println("Invalid Input");
+        }
+    }
+}
