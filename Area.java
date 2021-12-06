5a8aba14a5af184197ecf9a80a34e746c5958ea0
Benjamin Joseph <88529010+benjaminj26@users.noreply.github.com>
2021-12-06 12:40:15 +0530

    Correction

--- Area.java
+++ Area.java
@@ -7,24 +7,37 @@ public class Area {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
-		int length,breadth,base,height,radius;
+		int length,breadth;
+		float base,height,radius;
 		//Triangle
 		System.out.print("Input the base length of the triangle: ");
 		base = sc.nextInt();
 		System.out.print("Input the height of the triangle: ");
 		height = sc.nextInt();
-		System.out.println("Area of triangle = "+(((float)base*(float)height)/2));
+		System.out.println("Area of triangle = "+(area(base,height)));
 		//Rectangle
 		System.out.print("Input the length of the rectangle: ");
 		length = sc.nextInt();
 		System.out.print("Input the breadth of the rectangle: ");
 		breadth = sc.nextInt();
-		System.out.println("Area of the rectangle = "+(length*breadth));
+		System.out.println("Area of the rectangle = "+(area(length, breadth)));
 		//Circle
 		System.out.print("Input the radius of the circle: ");
 		radius = sc.nextInt();
-		System.out.println("Area of the circle = "+(((float)22/(float)7)*(float)radius*(float)radius));
+		System.out.println("Area of the circle = "+(area(radius)));
 		sc.close();
+	}
+	
+	static float area(float base, float height) {
+		return ((base*height)/2);
+	}
+	
+	static int area(int length, int breadth) {
+		return (length*breadth);
+	}
+	
+	static float area(float radius) {
+		return ((22f/7f)*radius*radius);
 	}
 
 }
