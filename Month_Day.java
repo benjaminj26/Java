8b235a8f4a1d43c866b46cd2f01bd76aa4df88fd
Benjamin Joseph <benjaminjoseph2002@gmail.com>
2021-12-08 23:28:26 +0530

    Fixed a bug

--- Month_Day.java
+++ Month_Day.java
@@ -12,32 +12,68 @@ public class Month_Day {
 		month.toLowerCase();
 		System.out.println("Input the year: ");
 		year = sc.nextInt();
-		switch(month) {
-			case "january": System.out.println("31");
-			break;
-			case "march": System.out.println("31");
-			break;
-			case "may": System.out.println("31");
-			break;
-			case "july": System.out.println("31");
-			break;
-			case "august": System.out.println("31");
-			case "october": System.out.println("31");
-			case "december": System.out.println("31");
-			break;
-			
-			case "february": System.out.println("28");
-			break;
-			
-			case "april": System.out.println("30");
-			case "june": System.out.println("30");
-			case "september": System.out.println("30");
-			case "november": System.out.println("30");
-			break;
-			
-			
-			default:System.out.println("Invalid Input");
+		if(isLeapYear(year))
+		{
+			switch(month) {
+				case "january": System.out.println("31");
+				case "march": System.out.println("31");
+				case "may": System.out.println("31");
+				case "july": System.out.println("31");
+				case "august": System.out.println("31");
+				case "october": System.out.println("31");
+				case "december": System.out.println("31");
+				break;
+				case "february": System.out.println("29");
+				break;
+				case "april": System.out.println("30");
+				case "june": System.out.println("30");
+				case "september": System.out.println("30");
+				case "november": System.out.println("30");
+				break;
+				default:System.out.println("Invalid Input");
+			}
+		}
+		else
+		{
+			switch(month) {
+				case "january": System.out.println("31");
+				case "march": System.out.println("31");
+				case "may": System.out.println("31");
+				case "july": System.out.println("31");
+				case "august": System.out.println("31");
+				case "october": System.out.println("31");
+				case "december": System.out.println("31");
+				break;
+				case "february": System.out.println("28");
+				break;
+				case "april": System.out.println("30");
+				case "june": System.out.println("30");
+				case "september": System.out.println("30");
+				case "november": System.out.println("30");
+				break;
+
+
+				default:System.out.println("Invalid Input");
+			}
 		}
 		sc.close();
+	}
+
+	static boolean isLeapYear(int year)
+	{
+		if(year%4 == 0 )
+		{
+			if(year%100 == 0)
+			{
+				if(year%400 == 0)
+					return true;
+				else
+					return false;
+			}
+			else
+				return true;
+		}
+		else
+			return false;
 	}
 }
