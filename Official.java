334a52cd3330b62331364a1b1efb4382f89a2fde
Benjamin Joseph <88529010+benjaminj26@users.noreply.github.com>
2021-12-10 17:05:17 +0530

    Updated

--- Official.java
+++ Official.java
@@ -10,7 +10,7 @@ class Employee{
 	int salary;
 	
 	public void printSalary() {
-		System.out.println("Salary of the employee = "+salary);
+		System.out.println("Salary: "+salary);
 	}
 	
 }
@@ -23,7 +23,7 @@ class Manager extends Employee{
 	String department;
 }
 
-public class Official {
+public class Inheritance {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -37,10 +37,12 @@ public class Official {
 		System.out.println("Enter the phone number of the officer: ");
 		off.phone = sc.nextInt();
 		System.out.println("Enter the address of the officer: ");
+		char ch = sc.next().charAt(0);
 		off.address = sc.nextLine();
 		System.out.println("Enter the salary of the officer: ");
 		off.salary = sc.nextInt();
 		System.out.println("Enter the specialization of the officer: ");
+		ch = sc.next().charAt(0);
 		off.specialization = sc.nextLine();
 		
 		System.out.println("Enter the name of the manager: ");
@@ -50,12 +52,32 @@ public class Official {
 		System.out.println("Enter the phone number of the manager: ");
 		man.phone = sc.nextInt();
 		System.out.println("Enter the address of the manager: ");
+		ch = sc.next().charAt(0);
 		man.address = sc.nextLine();
 		System.out.println("Enter the salary of the manager: ");
 		man.salary = sc.nextInt();
 		System.out.println("Enter the department of the manager: ");
+		ch = sc.next().charAt(0);
 		man.department = sc.nextLine();
 		sc.close();
+		
+		System.out.println();
+		System.out.println("Details of the Officer");
+		System.out.println("Name: "+off.name);
+		System.out.println("Age: "+off.age);
+		System.out.println("Phone Number: "+off.phone);
+		System.out.println("Address: "+off.address);
+		off.printSalary();
+		System.out.println("Specialization: "+off.specialization);
+		
+		System.out.println();
+		System.out.println("Details of Manager");
+		System.out.println("Name: "+man.name);
+		System.out.println("Age: "+man.age);
+		System.out.println("Phone Number: "+man.phone);
+		System.out.println("Address: "+man.address);
+		man.printSalary();
+		System.out.println("Department: "+man.department);
 	}
 
 }
