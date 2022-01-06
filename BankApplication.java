206e3b43086d26095875578bcac00bbfcb898de9
Benjamin Joseph <88529010+benjaminj26@users.noreply.github.com>
2022-01-06 14:59:16 +0530

    Updates

--- BankApplication.java
+++ BankApplication.java
@@ -6,13 +6,19 @@ class Customer{
 	int accountNumber;
 	int bankBalance;
 	String name;
-	Customer(int accountNumber, int bankBalance, String name){
-		this.accountNumber = accountNumber;
-		this.bankBalance = bankBalance;
-		this.name = name;
+	Customer(){
+		this.bankBalance = 0;
+	}
+	void getDetails() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter the Account Number: ");
+		this.accountNumber = sc.nextInt();
+		System.out.print("Enter the name: ");
+		sc.nextLine();
+		this.name = sc.nextLine();
 	}
 	void display() {
-		System.out.println("Account Number"+this.accountNumber);
+		System.out.println("Account Number: "+this.accountNumber);
 		System.out.println("Name: "+this.name);
 		System.out.println("Bank Balance: "+this.bankBalance);
 	}
@@ -20,6 +26,7 @@ class Customer{
 		System.out.print("Enter the amount to be deposited: ");
 		Scanner sc = new Scanner(System.in);
 		this.bankBalance += sc.nextInt();
+		System.out.println("The transaction has been finished successfully");
 	}
 	void withdraw() {
 		Scanner sc = new Scanner(System.in);
@@ -33,7 +40,14 @@ public class BankApplication {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Scanner sc = new Scanner(System.in);
+		int size;
+		System.out.print("Enter the number of customers: ");
+		size = sc.nextInt();
+		Customer customers = new Customer();
+		customers.getDetails();
+		customers.deposit();
+		customers.display();
 	}
 
 }
