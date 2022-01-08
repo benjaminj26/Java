8e59e1d549a4e2321d57a99ee0271a46c283829a
Benjamin Joseph <benjaminjoseph2002@gmail.com>
2022-01-08 13:21:58 +0530

    Finished Exceptions.java

--- Exceptions.java
+++ Exceptions.java
@@ -3,20 +3,25 @@ package Java;
 import java.util.Scanner;
 
 public class Exceptions {
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter Number1: ");
-		int num1 = sc.nextInt();
-		System.out.println("Enter Number2: ");
-		int num2 = sc.nextInt();
-		try {
-			int result = num1/num2;
-			System.out.println(num1+"/"+num2+"="+result);
-		}
-		catch(ArithmeticException e) {
-			System.out.println("Division by zero is not allowed");
-		}
-	}
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char choice;
+        do {
+            System.out.print("Enter Number1: ");
+            int num1 = sc.nextInt();
+            System.out.print("Enter Number2: ");
+            int num2 = sc.nextInt();
+            try {
+                int result = num1 / num2;
+                System.out.println(num1 + "/" + num2 + "=" + result);
+            }
+			catch (ArithmeticException e) {
+                System.out.println(e.getMessage());
+            }
+            System.out.println("End of the operation");
+            System.out.print("Do you want to continue?(y/n): ");
+            sc.nextLine();
+            choice = sc.next().charAt(0);
+        }while(choice == 'y');
+    }
 }
