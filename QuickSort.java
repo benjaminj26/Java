ee004622b587ef2653241263faf6f7c6a9b62fc4
Benjamin Joseph <benjaminjoseph2002@gmail.com>
2022-02-15 22:23:07 +0530

    Fixed a Bug

--- QuickSort.java
+++ QuickSort.java
@@ -10,7 +10,7 @@ public class QuickSort {
 
         System.out.println("Enter "+size+" elements:");
         int[] array = new int[size];
-        for(int i=0; i<size-1; ++i) {
+        for(int i=0; i<size; ++i) {
             array[i] = sc.nextInt();
         }
         quickSort(array, 0, size-1);
@@ -22,43 +22,38 @@ public class QuickSort {
         System.out.println();
     }
     protected static void quickSort(int[] array, int low, int upp) {
-        if(low < upp) {
-            int location = partition(array, low, upp);
-
-            quickSort(array, low, location-1);
-            quickSort(array, location+1, upp);
+        if(low >= upp) {
+            return;
         }
+        int location = partition(array, low, upp);
+
+        quickSort(array, low, location-1);
+        quickSort(array, location+1, upp);
     }
 
     protected static int partition(int[] array, int low, int upp) {
-        int start = low+1;
+        int start = low;
         int end = upp;
         int pivot = array[low];
 
-        while(array[start] <= pivot)
-        {
+        while(array[start] <= pivot && start < end) {
             start++;
         }
-
-        while (array[end] > pivot)
-        {
+        while(array[end] > pivot) {
             end--;
         }
-
-        if(start < end)
-        {
-            int tmp = array[start];
-            array[start] = array[end];
-            array[end] = tmp;
+        if(start < end) {
+            swap(array, start, end);
         }
-
-        else
-        {
-            int tmp = array[low];
-            array[low] = array[end];
-            array[end] = tmp;
+        else {
+                swap(array, end, low);
         }
-
         return end;
+    }
+
+    private static void swap(int[] array, int index1, int index2) {
+        int temp = array[index1];
+        array[index1] = array[index2];
+        array[index2] = temp;
     }
 }
